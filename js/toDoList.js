1eae1a68423a705cc950f7b196bf2da9baf1b519
Kangmin An <hkkm612@gmail.com>
2021-02-15 01:42:39 +0900

    Function-TodoList edit,delete

--- js/toDoList.js
+++ js/toDoList.js
@@ -4,6 +4,9 @@ const todoList = document.querySelector(".js-todoList");
 
 const TODOLIST = "todoList";
 
+const SHOWING_OFF = "showing-off";
+const SHOWING_ON = "showing-on";
+
 let todos = []; // fake DB
 
 function saveToDos() {
@@ -38,52 +41,130 @@ function handleDeleteBtn(event) {
   saveToDos();
 }
 
-function handleDoneBtn(event) {}
+function handleDoneBtn(event) {
+  const doneBtn = event.target;
+  const li = doneBtn.parentNode;
+}
 
-function handleEditBtn(event) {}
+function handleEditSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const input = form.querySelector("input");
+  const span = form.parentNode;
+  const li = span.parentNode;
+  const doneBtn = li.querySelector(".todo-doneBtn");
+  const editBtn = li.querySelector(".todo-editBtn");
+  const delBtn = li.querySelector(".todo-delBtn");
 
-function paintTodos(todo) {
+  const currentTxt = input.value;
+
+  input.removeEventListener("blur", handleEditBlur);
+  form.remove();
+
+  span.innerText = currentTxt;
+  doneBtn.classList.remove(SHOWING_OFF);
+  editBtn.classList.remove(SHOWING_OFF);
+  delBtn.classList.remove(SHOWING_OFF);
+
+  const updatedList = todos.map((todo) => {
+    if (todo.id === parseInt(li.id)) {
+      todo.text = currentTxt;
+    }
+    return todo;
+  });
+  todos = updatedList;
+  saveToDos();
+}
+
+function handleEditBlur(event) {
+  const editInput = event.target;
+  const editForm = editInput.parentNode;
+  const span = editForm.parentNode;
+  const li = span.parentNode;
+
+  const doneBtn = li.querySelector(".todo-doneBtn");
+  const editBtn = li.querySelector(".todo-editBtn");
+  const delBtn = li.querySelector(".todo-delBtn");
+
+  span.innerText = editInput.placeholder;
+  doneBtn.classList.remove(SHOWING_OFF);
+  editBtn.classList.remove(SHOWING_OFF);
+  delBtn.classList.remove(SHOWING_OFF);
+
+  editForm.remove();
+}
+
+function handleEditBtn(event) {
+  const editBtn = event.target;
+  const li = editBtn.parentNode;
+  const doneBtn = li.querySelector(".todo-doneBtn");
+  const delBtn = li.querySelector(".todo-delBtn");
+  const span = li.querySelector("span");
+
+  const form = document.createElement("form");
+  const input = document.createElement("input");
+
+  const currentTxt = span.innerText;
+
+  form.addEventListener("submit", handleEditSubmit);
+  input.placeholder = currentTxt;
+  form.appendChild(input);
+  input.addEventListener("blur", handleEditBlur);
+
+  span.innerText = "";
+  span.appendChild(form);
+  input.focus();
+
+  doneBtn.classList.add(SHOWING_OFF);
+  editBtn.classList.add(SHOWING_OFF);
+  delBtn.classList.add(SHOWING_OFF);
+}
+
+function paintTodos(todoObj) {
   const li = document.createElement("li");
   const deletebtn = document.createElement("button");
   const donebtn = document.createElement("button");
   const editbtn = document.createElement("button");
   const span = document.createElement("span");
-  const newID = createNewID();
 
   deletebtn.innerText = "✕";
+  deletebtn.classList.add("todo-delBtn");
   deletebtn.addEventListener("click", handleDeleteBtn);
 
-  donebtn.addEventListener("click", handleDoneBtn);
   donebtn.innerText = "✔︎";
+  donebtn.classList.add("todo-doneBtn");
+  donebtn.addEventListener("click", handleDoneBtn);
 
   editbtn.innerText = "✎";
+  editbtn.classList.add("todo-editBtn");
   editbtn.addEventListener("click", handleEditBtn);
 
-  span.innerText = todo;
+  span.innerText = todoObj.text;
 
   li.appendChild(donebtn);
   li.appendChild(span);
   li.appendChild(editbtn);
   li.appendChild(deletebtn);
-  li.id = newID;
+  li.id = todoObj.id;
 
   todoList.appendChild(li);
-
-  const todoObj = {
-    id: newID,
-    text: todo,
-    isDone: false,
-  };
-
-  todos.push(todoObj);
-  saveToDos();
 }
 
 function handleToDoSubmit(event) {
   event.preventDefault();
   const currentVal = todoInput.value;
-  paintTodos(currentVal);
+  const newID = createNewID();
+  const todoObj = {
+    id: newID,
+    text: currentVal,
+    isDone: false,
+  };
+
+  paintTodos(todoObj);
   todoInput.value = "";
+
+  todos.push(todoObj);
+  saveToDos();
 }
 
 function loadToDos() {
@@ -91,8 +172,9 @@ function loadToDos() {
   if (todols !== null) {
     const parsedTodols = JSON.parse(todols);
     parsedTodols.forEach((todo) => {
-      paintTodos(todo.text);
+      paintTodos(todo);
     });
+    todos = parsedTodols;
   }
 }
 
